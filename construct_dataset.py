30a6d3db4e20cd6802861414a541ad32753ed766
miles <mepste01@students.cs.ubc.ca>
2022-11-13 10:06:18 -0800

    add empty data manipulation files

---
+++ construct_dataset.py
@@ -0,0 +1,6 @@
+# for each station in stations
+# select good 6 hour windows
+# for each window
+# add slr and weather and meta data from that window as a row to overall dataset
+
+# save overall dataset
---
+++ download_era5.py
@@ -0,0 +1,7 @@
+# download stations data
+
+# for each station
+# download its data
+# put its start and endtime in stations data
+# download era5 data w/ appropriate variables at appropriate time and location 
+# save era5 data as csv
