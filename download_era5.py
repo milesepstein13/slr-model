beeeb0011ac521917691354844ef738d8640b17d
miles <mepste01@students.cs.ubc.ca>
2022-11-19 17:05:38 -0800

    start era download file

--- download_era5.py
+++ download_era5.py
@@ -1,7 +1,16 @@
+import numpy as np
+import pandas as pd
 # download stations data
+data = pd.read_csv("data/stations.csv")   
+stations = pd.DataFrame(data)
+
 
 # for each station
-# download its data
-# put its start and endtime in stations data
-# download era5 data w/ appropriate variables at appropriate time and location 
-# save era5 data as csv
+for station in stations.iterrows():
+    print(station)
+
+    # download its data
+
+    # put its start and endtime in stations data
+    # download era5 data w/ appropriate variables at appropriate time and location 
+    # save era5 data as csv
