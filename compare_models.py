a8242f1da35a1e389f83961d1e40712842d26e90
Miles Epstein <miles.epstein@comcast.net>
2023-03-21 11:05:58 -0700

    figures

--- compare_models.py
+++ compare_models.py
@@ -14,10 +14,10 @@ np.random.seed(0)
 def get_validation(dataset):
     values = dataset.values
     np.random.shuffle(values)
-    print(values.shape)
-    print(dataset.shape)
+    #print(values.shape)
+    #print(dataset.shape)
     dataset = pd.DataFrame(values[:, 1:], columns =dataset.columns[1:])
-    print(dataset)
+    #print(dataset)
     n = dataset.shape[0]
     split = int(8*n/10)
     dataset = dataset.iloc[split + 1:, :]
@@ -26,6 +26,7 @@ def get_validation(dataset):
 base_name = 'data/datasets/test_data_6_modified'
 
 # load in validation datasets (each dataset contains different variables)
+print("Loading datasets")
 dataset_names = ["", "_small", "_without_derived", "_without_meta", "_without_radiation", "_without_surface", "_without_temperature", "_without_upper", "_without_water", "_without_wind"]
 validation_datasets = dict()
 for name in dataset_names:
@@ -107,9 +108,9 @@ def predict_nws(dataset):
 
 def predict_nn(dataset, num):
     model = keras.models.load_model("models/"+str(num))
-    print(dataset)
+    #print(dataset)
     X = dataset.to_numpy()[:, :-1]
-    print(X)
+    #print(X)
     return(model.predict(X))
 
 # other prediction methods: ECCC, others from studies
@@ -136,71 +137,146 @@ def get_clusters(dataset):
     plt.clf()
     return clusters
 
+print("Making predictions...")
+
 predictions = pd.DataFrame().assign(recorded = validation_datasets[""].SLR, 
                                     ten = predict_ten(validation_datasets[""]), 
                                     meann = predict_mean(validation_datasets[""]),
                                     station_mean = predict_station_mean(validation_datasets[""]),
                                     kuchera = predict_kuchera(validation_datasets[""]),
-                                    nn = predict_nn(validation_datasets[""], 174),
+                                    nn_full = predict_nn(validation_datasets[""], 178),
+                                    nn_small = predict_nn(validation_datasets["_small"], 179),
+                                    nn_without_derived = predict_nn(validation_datasets["_without_derived"], 180),
+                                    nn_without_meta = predict_nn(validation_datasets["_without_meta"], 181),
+                                    nn_without_radiation = predict_nn(validation_datasets["_without_radiation"], 182),
+                                    nn_without_surface = predict_nn(validation_datasets["_without_surface"], 183),
+                                    nn_without_temperature = predict_nn(validation_datasets["_without_temperature"], 184),
+                                    nn_without_upper = predict_nn(validation_datasets["_without_upper"], 185),
+                                    nn_without_water = predict_nn(validation_datasets["_without_water"], 186),
+                                    nn_without_wind = predict_nn(validation_datasets["_without_wind"], 187),
                                     station = get_station_nums(validation_datasets[""]),
                                     cluster = get_clusters(validation_datasets[""]))
 
 print(predictions)
 
-
+# there's a better way to do this, but too late now
 SE = pd.DataFrame().assign(recorded = ((predictions.recorded-predictions.recorded)**2), 
                             ten = ((predictions.recorded-predictions.ten)**2), 
                             mean = ((predictions.recorded-predictions.meann)**2),
                             station_mean = ((predictions.recorded-predictions.station_mean)**2),
                             kuchera = ((predictions.recorded-predictions.kuchera)**2),
-                            nn = ((predictions.recorded-predictions.nn)**2),
+                            nn_full = ((predictions.recorded-predictions.nn_full)**2),
+                            nn_small = ((predictions.recorded-predictions.nn_small)**2),
+                            nn_without_derived = ((predictions.recorded-predictions.nn_without_derived)**2),
+                            nn_without_meta = ((predictions.recorded-predictions.nn_without_meta)**2),
+                            nn_without_radiation = ((predictions.recorded-predictions.nn_without_radiation)**2),
+                            nn_without_surface = ((predictions.recorded-predictions.nn_without_surface)**2),
+                            nn_without_temperature = ((predictions.recorded-predictions.nn_without_temperature)**2),
+                            nn_without_upper = ((predictions.recorded-predictions.nn_without_upper)**2),
+                            nn_without_water = ((predictions.recorded-predictions.nn_without_water)**2),
+                            nn_without_wind = ((predictions.recorded-predictions.nn_without_wind)**2),
+                            station = predictions.station,
+                            cluster = predictions.cluster)
+
+AE = pd.DataFrame().assign(recorded = (np.abs(predictions.recorded-predictions.recorded)), 
+                            ten = (np.abs(predictions.recorded-predictions.ten)), 
+                            mean = (np.abs(predictions.recorded-predictions.meann)),
+                            station_mean = (np.abs(predictions.recorded-predictions.station_mean)),
+                            kuchera = (np.abs(predictions.recorded-predictions.kuchera)),
+                            nn_full = (np.abs(predictions.recorded-predictions.nn_full)),
+                            nn_small = (np.abs(predictions.recorded-predictions.nn_small)),
+                            nn_without_derived = (np.abs(predictions.recorded-predictions.nn_without_derived)),
+                            nn_without_meta = (np.abs(predictions.recorded-predictions.nn_without_meta)),
+                            nn_without_radiation = (np.abs(predictions.recorded-predictions.nn_without_radiation)),
+                            nn_without_surface = (np.abs(predictions.recorded-predictions.nn_without_surface)),
+                            nn_without_temperature = (np.abs(predictions.recorded-predictions.nn_without_temperature)),
+                            nn_without_upper = (np.abs(predictions.recorded-predictions.nn_without_upper)),
+                            nn_without_water = (np.abs(predictions.recorded-predictions.nn_without_water)),
+                            nn_without_wind = (np.abs(predictions.recorded-predictions.nn_without_wind)),
                             station = predictions.station,
                             cluster = predictions.cluster)
 print(SE)
 
+#TODO: other error metrics
+
 MSE = SE.mean()
-print("Overall:")
+print("Overall MSE: ")
 print(MSE)
+
+MAE = AE.mean()
+print("Overall MAE: ")
+print(MAE)
+
+RMSE = np.sqrt(MSE)
+print("overall RMSE:")
+print(RMSE)
 
 
 SE_stations = SE.groupby('station')
 MSE_locations = SE_stations.mean()
 MSE_locations['count'] = SE_stations.count().recorded
-print("Locations:")
+print("MSE Locations:")
 print(MSE_locations)
+
+RMSE_stations = np.sqrt(SE_stations.mean())
+RMSE_stations['count'] = SE_stations.count().recorded
+
+
+AE_stations = AE.groupby('station')
+MAE_stations = AE_stations.mean()
+MAE_stations['count'] = AE_stations.count().recorded
+
 
 
 SE_clusters = SE.groupby('cluster')
 MSE_clusters = SE_clusters.mean()
 MSE_clusters['count'] = SE_clusters.count().recorded
-print("Clusters:")
+print("MSE Clusters:")
 print(MSE_clusters)
 
+RMSE_clusters = np.sqrt(SE_clusters.mean())
+RMSE_clusters['count'] = SE_clusters.count().recorded
 
+AE_clusters = AE.groupby('cluster')
+MAE_clusters = AE_clusters.mean()
+MAE_clusters['count'] = AE_clusters.count().recorded
+
+
+print("Saving data...")
 # save data
 predictions.to_csv('outputs/predictions.csv')
 MSE.to_csv('outputs/overall_mse.csv')
 MSE_locations.to_csv('outputs/station_mse.csv')
 MSE_clusters.to_csv('outputs/cluster_mse.csv')
 
+RMSE.to_csv('outputs/overall_rmse.csv')
+RMSE_stations.to_csv('outputs/station_rmse.csv')
+RMSE_clusters.to_csv('outputs/cluster_rmse.csv')
+
+MAE.to_csv('outputs/overall_mae.csv')
+MAE_stations.to_csv('outputs/station_mae.csv')
+MAE_clusters.to_csv('outputs/cluster_mae.csv')
+
+
 # put tables to latex:
-print(MSE.to_latex())
-print(MSE_locations.to_latex())
-print(MSE_clusters.to_latex())
+#print(MSE.to_latex())
+#print(MSE_locations.to_latex())
+#print(MSE_clusters.to_latex())
 
 # make figures
 # not sure what figures to make? Should put station-by-station data on a map. Ask rosie what
 
-def make_maps(MSE_locations, lats, lons):
-    for column in MSE_locations:
+def make_maps(value_locations, lats, lons, errortype, short_errortype):
+    for column in value_locations:
         if (column != 'recorded') and (column != 'cluster') and (column != 'count'):
-            plt.scatter(lons, lats, c = MSE_locations[column], s=MSE_locations['count'], cmap='rainbow')
+            plt.scatter(lons, lats, c = value_locations[column], s=value_locations['count'], cmap='rainbow')
 
-            plt.title("Mean Test Squared Error With " + column + " Method for SLR Prediction")
+            plt.title("Mean Test " + errortype + " With " + column + " Method for SLR Prediction")
             plt.colorbar()
             plt.xlabel("lon")
             plt.ylabel("lat")
-            plt.savefig('outputs/figs/' + column + '.png')
+            
+            plt.savefig('outputs/figs/' + short_errortype + column + '.png')
             plt.clf()
 
 
@@ -216,4 +292,8 @@ def get_lat_lon(dataset):
 
 
 (lats, lons) = get_lat_lon(validation_datasets[''])
-make_maps(MSE_locations, lats, lons)
+
+# do for each
+make_maps(MSE_locations, lats, lons, 'mean squared error', '_mse_')
+make_maps(RMSE_stations, lats, lons, 'root mean squared error', '_rmse_')
+make_maps(MAE_stations, lats, lons, 'mean absolute error', '_mae_')
--- explore_data.py
+++ explore_data.py
@@ -1,59 +1,128 @@
+import matplotlib as mpl
 import matplotlib.pyplot as plt
 
 import numpy as np
 from numpy import loadtxt
+import pandas as pd
+from scipy import stats
 
-filename = "data/datasets/" + "test_data_4_modified.csv"
+filename = "data/datasets/" + "test_data_4.csv"
  
 # load the dataset
-dataset = loadtxt(filename, delimiter=',', skiprows=1)
+
+# TODO: make scale of axes same for all plots, fix bin width artifact
+dataset = pd.read_csv(filename)
+print(dataset)
+
+def get_station_nums(dataset):
+    return stats.rankdata(dataset.latitude, method='dense')
+
+def get_clusters(dataset):
+    clusters = np.zeros(len(dataset.latitude))
+    for i in range(len(dataset.latitude)):
+        lat = dataset.latitude.iloc[i]
+        lon = dataset.longitude.iloc[i]
+        if (lon < -127 or (lon < -122.5 and lat < 54.5) or (lon < -120 and lat < 51)):
+            clusters[i] = 1
+        else:
+            clusters[i] = 2
+    plt.scatter(dataset.longitude, dataset.latitude, c = clusters, cmap='rainbow')
+
+    plt.title("Regions")
+    plt.colorbar()
+    plt.xlabel("lon")
+    plt.ylabel("lat")
+    plt.savefig('regions.png')
+    plt.clf()
+    return clusters
+
+plt.hist2d(dataset.pc_increase, dataset.sd_increase, norm=mpl.colors.LogNorm(), bins=50)
+plt.xlabel("Cumulative Precip Increase (mm)")
+plt.ylabel("Snow Depth Increase (cm)")
+plt.title('PC and SD increases for whole dataset')
+plt.colorbar()
+plt.savefig('pc_sd_plots/pc_sd_density.png')
+plt.clf()
+
+dataset = dataset.assign(cluster = get_clusters(dataset), station = get_station_nums(dataset))
+print(dataset)
+
+plt.hist2d(dataset[dataset.cluster == 1].pc_increase, dataset[dataset.cluster == 1].sd_increase, norm=mpl.colors.LogNorm(), bins=50)
+plt.xlabel("Cumulative Precip Increase (mm)")
+plt.ylabel("Snow Depth Increase (cm)")
+plt.title('PC and SD increases for coast range')
+plt.colorbar()
+plt.savefig('pc_sd_plots/pc_sd_density_coast.png')
+plt.clf()
+
+plt.hist2d(dataset[dataset.cluster == 2].pc_increase, dataset[dataset.cluster == 2].sd_increase, norm=mpl.colors.LogNorm(), bins=50)
+plt.xlabel("Cumulative Precip Increase (mm)")
+plt.ylabel("Snow Depth Increase (cm)")
+plt.title('PC and SD increases for interior')
+plt.colorbar()
+plt.savefig('pc_sd_plots/pc_sd_density_interior.png')
+plt.clf()
+
+for i in range(1, 66):
+    print("i: " + str(i))
+    plt.hist2d(dataset[dataset.station == i].pc_increase, dataset[dataset.station == i].sd_increase, norm=mpl.colors.LogNorm(), bins=20)
+    plt.xlabel("Cumulative Precip Increase (mm)")
+    plt.ylabel("Snow Depth Increase (cm)")
+    plt.title('PC and SD increases for station ' + str(i))
+    plt.colorbar()
+    plt.savefig('pc_sd_plots/pc_sd_density_station_' + str(i) + '.png')
+    plt.clf()
+
+print(np.max(dataset.station))
+print(np.min(dataset.station))
+
 # split into input (X) and output (y) variables
+if False:
+    slrs = dataset[:, -1]
+    lats = dataset[:, -4]
+    lons = dataset[:, -3]
+    elevs = dataset[:, -5]
 
-slrs = dataset[:, -1]
-lats = dataset[:, -4]
-lons = dataset[:, -3]
-elevs = dataset[:, -5]
+    lat = 0
+    lon = 0
+    count = 0
+    sum = 0
+    out = [0, 0, 0, 0, 0, 0]
 
-lat = 0
-lon = 0
-count = 0
-sum = 0
-out = [0, 0, 0, 0, 0, 0]
+    for i in range(len(slrs)):
+        if ((lats[i] != lat) ):
+            print("new location " + str(i))
+            if lat != 0:
+                slr = sum/count
+                out = np.vstack([out, [lat, lon, slr, count, lat-lon, elev]])
+                count = 0
+                sum = 0
+            lat = lats[i]
+            lon = lons[i]
+            elev = elevs[i]
+        sum = sum + slrs[i]
+        count = count + 1
+    out = out[1:, :]
+    print(out)
 
-for i in range(len(slrs)):
-    if ((lats[i] != lat) ):
-        print("new location " + str(i))
-        if lat != 0:
-            slr = sum/count
-            out = np.vstack([out, [lat, lon, slr, count, lat-lon, elev]])
-            count = 0
-            sum = 0
-        lat = lats[i]
-        lon = lons[i]
-        elev = elevs[i]
-    sum = sum + slrs[i]
-    count = count + 1
-out = out[1:, :]
-print(out)
+#plt.scatter(out[:, 1], out[:, 0], c = out[:, 2], s=out[:, 3]/2, cmap='rainbow')
 
-plt.scatter(out[:, 1], out[:, 0], c = out[:, 2], s=out[:, 3]/2, cmap='rainbow')
-
-plt.title("Mean SLR by location")
-plt.colorbar()
-plt.xlabel("lon")
-plt.ylabel("lat")
-plt.savefig('SLR_locations.png')
-plt.clf()
+#plt.title("Mean SLR by location")
+#plt.colorbar()
+#plt.xlabel("lon")
+#plt.ylabel("lat")
+#plt.savefig('SLR_locations.png')
+#plt.clf()
 
 
-plt.scatter(out[:, 4], out[:, 5], c = out[:, 2], s=out[:, 3]/2, cmap='rainbow')
+#plt.scatter(out[:, 4], out[:, 5], c = out[:, 2], s=out[:, 3]/2, cmap='rainbow')
 
-plt.title("Mean SLR by Northeasternness and elevation")
-plt.colorbar()
-plt.xlabel("northeasternness (lat-lon)")
-plt.ylabel("elevation")
-plt.savefig('SLR_NE_elev.png')
-plt.clf()
+#plt.title("Mean SLR by Northeasternness and elevation")
+#plt.colorbar()
+#plt.xlabel("northeasternness (lat-lon)")
+#plt.ylabel("elevation")
+#plt.savefig('SLR_NE_elev.png')
+#plt.clf()
 #plt.hist(dataset[:, -1], bins=20)
 #plt.title("SLR Histogram")
 #plt.xlabel("SLR")
